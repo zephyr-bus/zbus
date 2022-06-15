69a24ef81c7e6cfbf4c9b4a23dd41b4a8e77276e
Rodrigo Peixoto <rodrigopex@gmail.com>
2022-06-15 11:34:46 -0300

    Revert "Revert "Add dynamic channels to zbus""

---
+++ tests/dyn_channel/src/core.c
@@ -0,0 +1,22 @@
+/**
+ * @file      core.c
+ * @brief     Header of
+ * @date      Sun Nov 28 18:03:41 2021
+ * @author    Rodrigo Peixoto
+ * @copyright MIT
+ *
+ * This module
+ */
+
+#include "zbus.h"
+
+#include <logging/log.h>
+LOG_MODULE_DECLARE(zbus, CONFIG_ZBUS_LOG_LEVEL);
+
+ZBUS_SUBSCRIBER_REGISTER(core, 10);
+
+void core_thread(void)
+{
+}
+
+K_THREAD_DEFINE(core_thread_id, 1024, core_thread, NULL, NULL, NULL, 3, 0, 0);
