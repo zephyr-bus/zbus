bcae962a062eadf7046fab3ba73d1b466d714a91
Rodrigo Peixoto <rodrigopex@edge.ufal.br>
2022-07-17 11:57:40 -0300

    Fix the channel declaration at the tests
    Signed-off-by: Rodrigo Peixoto <rodrigopex@edge.ufal.br>

--- tests/extension/include/zbus_channels.h
+++ tests/extension/include/zbus_channels.h
@@ -2,6 +2,7 @@ ZBUS_CHANNEL(version,              /* Name */
              false,                /* On changes only */
              true,                 /* Read only */
              struct version,       /* Message type */
+             NULL,                 /* Validator */
              ZBUS_OBSERVERS_EMPTY, /* observers */
              ZBUS_MSG_INIT(.major = 0, .minor = 1,
                            .build = 1023) /* Initial value major 0, minor 1, build 1023 */
@@ -11,6 +12,7 @@ ZBUS_CHANNEL(sensor_data,          /* Name */
              true,                 /* On changes only */
              false,                /* Read only */
              struct sensor_data,   /* Message type */
+             NULL,                 /* Validator */
              ZBUS_OBSERVERS_EMPTY, /* observers */
              ZBUS_MSG_INIT(0)      /* Initial value {0} */
 )
@@ -19,6 +21,7 @@ ZBUS_CHANNEL(start_measurement,          /* Name */
              false,                      /* On changes only */
              false,                      /* Read only */
              struct action,              /* Message type */
+             NULL,                       /* Validator */
              ZBUS_OBSERVERS(peripheral), /* observers */
              ZBUS_MSG_INIT(false)        /* Initial value */
 )
@@ -27,6 +30,7 @@ ZBUS_CHANNEL(finish,               /* Name */
              false,                /* On changes only */
              false,                /* Read only */
              struct action,        /* Message type */
+             NULL,                 /* Validator */
              ZBUS_OBSERVERS_EMPTY, /* observers */
              ZBUS_MSG_INIT(false)  /* Initial value */
 )
