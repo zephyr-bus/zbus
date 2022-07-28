e74361da9239ce202830d924169520c79fb866ac
Rodrigo Peixoto <rodrigopex@edge.ufal.br>
2022-07-28 10:36:04 -0300

    Add tests for user data
    Signed-off-by: Rodrigo Peixoto <rodrigopex@edge.ufal.br>

---
+++ tests/user_data/include/zbus_channels.h
@@ -0,0 +1,19 @@
+ZBUS_CHAN_DEFINE(
+    version,              /* Name */
+    false,                /* On changes only */
+    true,                 /* Read only */
+    struct version,       /* Message type */
+    NULL,                 /* Validator */
+    ZBUS_OBSERVERS_EMPTY, /* observers */
+    ZBUS_MSG_INIT(.major = 0, .minor = 1,
+           .build = 2) /* Initial value major 0, minor 1, build 1023 */
+)
+ZBUS_CHAN_DEFINE(
+    regular_chan,                    /* Name */
+    false,                /* On changes only */
+    false,                 /* Read only */
+    struct foo_msg,       /* Message type */
+    NULL,                 /* Validator */
+    ZBUS_OBSERVERS(foo_listener, foo_subscriber), /* observers */
+    ZBUS_MSG_INIT(0)      /* Initial value major 0, minor 1, build 1023 */
+)
